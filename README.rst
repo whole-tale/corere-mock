53aa841bf9a236e0c360e88b0cee16a85ba071ae
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2021-09-13 20:59:34 -0500

    Initial import

---
+++ README.rst
@@ -0,0 +1,39 @@
+tracingfs
+#########
+
+|GitHub Project| |nsf-badge|
+
+Description
+===========
+
+A mock CORERE integration
+
+Installation
+============
+
+.. code-block:: shell
+
+    virtualenv -p /usr/bin/python3.8 venv  # at least 3.8 required
+    . ./venv/bin/activate
+    pip install -r requirements.txt
+
+Usage
+=====
+
+.. code-block:: shell
+
+    export GIRDER_API_KEY=...  # assumes you have girder.local running and created the key
+    python workflow.py
+
+Acknowledgements
+================
+
+This material is based upon work supported by the National Science Foundation under Grant No. OAC-1541450.
+
+.. |GitHub Project| image:: https://img.shields.io/badge/GitHub--blue?style=social&logo=GitHub
+   :target: https://github.com/whole-tale/tracingfs
+
+.. |nsf-badge| image:: https://img.shields.io/badge/NSF-154150-blue.svg
+    :target: https://www.nsf.gov/awardsearch/showAward?AWD_ID=1541450
+    :alt: NSF Grant Badge
+
---
+++ workflow.py
@@ -0,0 +1,161 @@
+import datetime
+import json
+import os
+import tempfile
+import threading
+import time
+from hashlib import md5
+
+from pathlib import Path
+
+import sseclient
+from girder_client import GirderClient
+
+
+class InstanceStatus:
+    LAUNCHING = 0
+    RUNNING = 1
+    ERROR = 2
+
+
+def md5sum(filename, buf_size=8192):
+    m = md5()
+    # the with statement makes sure the file will be closed
+    if filename.is_dir():
+        return
+    with open(filename, "rb") as f:
+        # We read the file in small chunk until EOF
+        data = f.read(buf_size)
+        while data:
+            # We had data to the md5 hash
+            m.update(data)
+            data = f.read(buf_size)
+    # We return the md5 hash in hex
+    return m.hexdigest()
+
+
+def event_listener(gc):
+    stream = gc.sendRestRequest(
+        "GET",
+        "/notification/stream",
+        stream=True,
+        headers={"Accept": "text/event-stream"},
+        jsonResp=False,
+        parameters={"since": int(datetime.datetime.now().timestamp())},
+    )
+    client = sseclient.SSEClient(stream)
+    for event in client.events():
+        data = json.loads(event.data)
+        if data["type"] == "wt_progress":
+            progress = int(data["data"]["current"] / data["data"]["total"] * 100.0)
+            msg = (
+                "  -> event received:"
+                f" msg = {data['data']['message']}"
+                f" status = {data['data']['state']}"
+                f" progress = {progress}%"
+            )
+            print(msg)
+
+
+class Manuscript:
+    """Pseudo core2 <-> WT interface.
+
+    We are going to map Manuscript to Tale and Submission to Version.
+    """
+
+    def __init__(self, api_url="https://girder.local.wholetale.org/api/v1"):
+        self.gc = GirderClient(apiUrl=api_url)
+        self.gc.authenticate(apiKey=os.environ.get("GIRDER_API_KEY"))
+        self.tale = self.create_tale()
+        self.sse_handler = threading.Thread(
+            target=event_listener, args=(self.gc,), daemon=False
+        )
+        self.sse_handler.start()
+
+    def default_image(self):
+        images = self.gc.get("/image", parameters={"text": "Jupyter"})
+        return images[0]
+
+    def create_tale(self, image=None):
+        if image is None:
+            image = self.default_image()
+
+        tale = self.gc.post("/tale", json={"imageId": image["_id"], "dataSet": []})
+        return tale
+
+    def create_submission(self, name=None, path=None):
+        """
+        path needs to point to a directory with submission files
+        """
+        if path is None:
+            path = Path(os.path.dirname(__file__)) / "example_submission"
+
+        # upload path
+        for fname in path.iterdir():
+            self.gc.uploadFileToFolder(self.tale["workspaceId"], fname)
+
+        # Finalize an immutable "submission"
+        parameters = {"taleId": self.tale["_id"]}
+        if name is not None:
+            parameters["name"] = name
+        version = self.gc.post("/version", parameters=parameters)
+        return version
+
+    def run(self, submissionId=None):
+        if submissionId is not None:
+            print("We would revert to that version. Pass now")
+        instance = self.gc.post("/instance", parameters={"taleId": self.tale["_id"]})
+        while instance["status"] == InstanceStatus.LAUNCHING:
+            time.sleep(2)
+            instance = self.gc.get(f"/instance/{instance['_id']}")
+        return instance
+
+    def stop(self, instance):
+        self.gc.delete(f"/instance/{instance['_id']}")
+
+    def download_submission(self, folder_id=None):
+        if folder_id is None:
+            folder_id = self.tale["workspaceId"]  # otherwise it should be version
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            print("[*] Created temporary directory for submission", tmpdirname)
+            self.gc.downloadFolderRecursive(folder_id, tmpdirname)
+            print(
+                "[*] Comparing files pre/post execution "
+                "(ultimately can happen on the backend)"
+            )
+            new = Path(tmpdirname)
+            old = Path(__file__).parent / "example_submission"
+
+            new_files = set(_.name for _ in new.iterdir())
+            old_files = set(_.name for _ in old.iterdir())
+
+            if diff := new_files - old_files:
+                print("    New files:")
+                for name in diff:
+                    print(f"      -> {name}")
+            if diff := old_files - new_files:
+                print("    Removed files:")
+                for name in diff:
+                    print(f"      -> {name}")
+            for name in new_files & old_files:
+                new_sum = md5sum(new / name)
+                old_sum = md5sum(old / name)
+                if new_sum != old_sum:
+                    print(f"File {name} was modified!!! (md5sum differs)")
+        print("[*] Cleaning up...")
+
+
+print("[*] Creating a new Manuscript")
+manuscript = Manuscript()
+print("[*] Creating submission and uploading data")
+manuscript.create_submission(name="Submission no. 1")
+print("[*] Starting Jupyter notebook (this may take a while...)")
+binder = manuscript.run()
+print("----")
+print(f"  Open your browser and go to: {binder['url']}")
+print("   Make sure to run 'run_me.ipynb'")
+input("   After you're done with notebook press Enter to continue...")
+manuscript.stop(binder)
+manuscript.download_submission()
+print("Press CTRL-C to exit")
+manuscript.sse_handler.join()
